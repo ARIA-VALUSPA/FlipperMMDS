f47b58a7d9c9367100e47c570b69ecd5fce06a70
Tommy Nilsson <tommy nilsson>
2016-08-04 14:54:10 +0200

    Added partial responses file and some updates to TdawgManager

--- ExampleManagers/ExampleManagers/src/eu/aria/dialogue/managers/TdawgManager.java
+++ ExampleManagers/ExampleManagers/src/eu/aria/dialogue/managers/TdawgManager.java
@@ -1,7 +1,6 @@
-/*
+package eu.aria.dialogue.managers;/*
 by Kevin Bowden
  */
-package eu.aria.dialogue.managers;
 
 import edu.stanford.nlp.trees.Tree;
 import edu.stanford.nlp.trees.TypedDependency;
@@ -13,8 +12,10 @@ import hmi.flipper.defaultInformationstate.DefaultRecord;
 import hmi.flipper.informationstate.List;
 import hmi.flipper.informationstate.Record;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Locale;
 import java.util.Map;
 
 /**
@@ -24,14 +25,41 @@ import java.util.Map;
 
 
 
-
  public class TdawgManager extends DefaultManager {
     private String userUtterancePath = "$userstates.utterance";
     private String userTdawgPath = "$userstates.utterance.tdawg";
     private String userposPath = "$userstates.utterance.pos";
 
+    private String agentposPath = "$agentstates.utterance";
 
-    public static String[] country = {"germany","america","england"}; ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    private String countryPath = "$userstates.utterance.pos";
+    //ArrayList <String> nations = new ArrayList<>();
+
+////////////////////////////////////////////GENERATES ARRAY OF COUNTRY NAMES.
+
+//    public static void main(String[] args) {
+//
+//        //returns array of all locales
+//        Locale locales[] = SimpleDateFormat.getAvailableLocales();
+//        ArrayList <String> nations = new ArrayList<>();
+//
+//        for (int i = 0; i < locales.length; i++) {
+//            nations.add(locales[i].getDisplayCountry());
+//        }
+//        System.out.println("............--........."+nations.get(6));
+//        System.out.println("............--........."+nations);
+//        System.out.println("............--........."+nations);
+//    }
+
+
+
+
+
+//    Record countryUtterance = getIS().getRecord(countryPath);
+
+
+
+    //public static String[] nations = {"germany","america","england","netherlands","australia"}; ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     public TdawgManager(DefaultRecord is) {
         super(is);
@@ -53,8 +81,21 @@ import java.util.Map;
     public void process() {
         super.process();
 
+        //returns array of all locales
+        Locale locales[] = SimpleDateFormat.getAvailableLocales();
+        ArrayList <String> nations = new ArrayList<>();///////////////////////////////////////////////////////////////////////////
+        for (int i = 0; i < locales.length; i++) {
+            nations.add(locales[i].getDisplayCountry());
+        }
+        //if(nations.contains("Czech Republic")){System.out.println("Sweden!!!");}
+        //System.out.println("Nations..................................."+nations);
 
         Record utterance = getIS().getRecord(userUtterancePath);
+
+
+
+
+
 
         if (utterance == null) {
             utterance = new DefaultRecord();
@@ -84,8 +125,13 @@ import java.util.Map;
                 if (posUtterance.getList("nouns") == null) {
                     posUtterance.set("nouns", new DefaultList());
                 }
+
                 if (tdawgUtterance.getString("exclaim") == null) {
-                    tdawgUtterance.set("exclaim", "");
+                    tdawgUtterance.set("exclaim", "false");
+                }
+
+               if (posUtterance.getString("exclaim") == null) {
+                   posUtterance.set("exclaim", "false");
                 }
 
             String userSay = utterance.getString("text");
@@ -93,7 +139,9 @@ import java.util.Map;
             if (userSay != null && !utterance.getString("consumed").equals("true")) {
 
                // if(userSay.contains("germany")){}
+//                System.out.println("agent says string contains the following text............................................: "+agentsays);
 
+                //if(nations contains noun){trigger response;}
 
                 if(userSay.endsWith("!")){
                     tdawgUtterance.set("exclaim", "true");
@@ -101,8 +149,31 @@ import java.util.Map;
                 } else {
                     tdawgUtterance.set("exclaim", "false");
                 }
+
+                ////////////////////////////////
+//                for(int i = 0; i < nations.length; i++) {
+//
+//                    if (userSay.contains(nations.get(i))){}
+//
+//                }
+
+//                Record posUtterance2 = getIS().getRecord(agentposPath);//////////////////////////////////////////////////////////////////
+//
+//                String agentresponse = posUtterance2.getString("text");
+//                System.out.println("This should containt the agent response............................................ "+agentresponse);
+//
+//                  if(agentresponse.endsWith("!")){
+//                    tdawgUtterance.set("exclaim", "false");
+//                }
+
                 List nounsList = posUtterance.getList("nouns");
             }
         }
     }
+
+
+
+
+
+
  }
